1e9c4297b9266b38f5ddb6e6a3878ed2c94d83f6
aachan <aaronlun2005@gmail.com>
2018-07-07 13:40:26 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -98,15 +98,36 @@ app.get('/pagecount', function (req, res) {
 app.get('/test', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-var url = "http://v.163.com/special/opencourse/englishs1.html"
-var test = "";
- request(url, (err, res, body) => {
-  const $ = cheerio.load(body)
-  
-     test +=  $("a.downbtn")[0];
 
-})
-  res.send('{ text: ' + test + '}');
+ request({
+    url: "http://www.cwb.gov.tw/V7/modules/MOD_EC_Home.htm", // 中央氣象局網頁
+    method: "GET"
+  }, function (error, response, body) {
+    if (error || !body) {
+      return;
+    }
+    const $ = cheerio.load(body); // 載入 body
+    const result = []; // 建立一個儲存結果的容器
+    const table_tr = $(".BoxTable tr"); // 爬最外層的 Table(class=BoxTable) 中的 tr
+
+    for (let i = 1; i < table_tr.length; i++) { // 走訪 tr
+      const table_td = table_tr.eq(i).find('td'); // 擷取每個欄位(td)
+      const time = table_td.eq(1).text(); // time (台灣時間)
+      const latitude = table_td.eq(2).text(); // latitude (緯度)
+      const longitude = table_td.eq(3).text(); // longitude (經度)
+      const amgnitude = table_td.eq(4).text(); // magnitude (規模)
+      const depth = table_td.eq(5).text(); // depth (深度)
+      const location = table_td.eq(6).text(); // location (位置)
+      const url = table_td.eq(7).text(); // url (網址)
+      // 建立物件並(push)存入結果
+      result.push(Object.assign({ time, latitude, longitude, amgnitude, depth, location, url }));
+    }
+    // 在終端機(console)列出結果
+    console.log(result);
+   
+  });
+  
+  res.send( JSON.stringify(result));
 	
 });
 
