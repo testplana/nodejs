8581bd08249c2fbdde90542a3cd50fbfa42ea572
aachan <aaronlun2005@gmail.com>
2018-07-07 12:50:22 +0800

    server update

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
+
+var cheerio = require('cheerio');
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
