63ec02aa38dd79180b380c315f22fa73181a6971
aachan <aaronlun2005@gmail.com>
2018-07-10 21:21:43 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -78,6 +78,13 @@ app.get('/', function (req, res) {
   }
 });
 
+
+app.get('/news', function (req, res) {
+   res.render('news.html', { pageCountMessage : null });
+   
+});
+
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
