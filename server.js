f343ace998f3560a21af3531b5b8545c5c9194dc
aachan <aaronlun2005@gmail.com>
2018-07-10 14:35:02 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -101,7 +101,11 @@ app.get('/newscontent', function (req, res) {
     initDb(function(err){});
   }
   if (db) {
-console.log(db.collection('news').find({datetime: { $gt: 0 }}));
+
+  db.collection('news').find().limit(10).toArray(
+	function(err, docs){
+		console.log(docs);
+	});
 	db.collection('news').count(function(err, count ){
       res.send('{ newCount: ' + count + '}');
     });
