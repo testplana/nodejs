819bf13574455b9f1c31f34635a94732df85b14d
aachan <aaronlun2005@gmail.com>
2018-07-26 01:26:03 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -144,8 +144,9 @@ db.collection('news').find().limit(100).sort( { datetime: -1 } ).toArray(
 				]}  
 			).sort( { datetime: -1 } ).toArray(
 			function(err, stockdocs){		
-				for (j = 0 ; j < stockdocs.length;j++){					
-					stockList.push(Object.assign({ stockdocs[j] }));
+				for (j = 0 ; j < stockdocs.length;j++){		
+					temp = stockdocs[j];
+					stockList.push(Object.assign({ temp }));
 				}
 				
 			});				
